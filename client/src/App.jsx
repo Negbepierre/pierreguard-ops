b39a1ff0cb744e124cbabe71918713e7e0d11b4c
EKHOMALOMEN INEGBENOSE PIERRE <negbe_pierre@Mac.broadband>
2026-04-04 21:02:38 +0100

    update API URL to production backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Login from './components/Login'
 import Dashboard from './components/Dashboard'
 import ScanLanding from './components/ScanLanding'
 
-const API_URL = 'http://127.0.0.1:5001'
+const API_URL = 'https://pierreguard-ops.onrender.com'
 
 function App() {
   const [user, setUser] = useState(null)
